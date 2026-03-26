625b7a15b5c41841bdc508d797bd9fc230680b00
makyuplier <mcgybear@gmail.com>
2026-03-27 05:04:38 +0800

    minor scrolling fixes

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,12 +1,16 @@
+// ScrollToTop.jsx
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    // Only scroll to top if the hash is empty
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 
   return null;
 }
